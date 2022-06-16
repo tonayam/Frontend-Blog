b9fea69eb1670abc5a1ff72e36d0387d560eaf01
tonayam <tonayam99@gmail.com>
2022-06-16 06:56:10 +0100

    Updates

--- js/app.js
+++ js/app.js
@@ -6,11 +6,12 @@ const submitBtn = document.querySelector(`.submit-btn`);
 const inputTitle = document.getElementById(`title`);
 const inputBody = document.getElementById(`body`);
 const imageURL = document.querySelector(`#image-url`);
-console.log(imageURL);
 const overlay = document.querySelector(`.overlay`);
 const div = document.querySelector(`.latest-post`);
 const latestPost = document.querySelector(`.latestPost`);
 const bgImages = document.querySelector(`.read-img`);
+const form = document.querySelector(`.form`);
+const preloader = document.querySelector(`.preloader`);
 let defaultItem = 0;
 
 // CUSTOM ARRAYS
@@ -40,6 +41,10 @@ const dateArray = [
   { date: `15th April, 2022` },
 ];
 
+// PRELOADER
+window.addEventListener(`load`, () => {
+  preloader.classList.add(`hide-preloader`);
+});
 // OPEN MODAL
 createPost.addEventListener(`click`, () => {
   overlay.classList.add(`show-modal`);
@@ -68,32 +73,58 @@ submitBtn.addEventListener(`click`, (e) => {
     .then((data) => {
       console.log(data);
       myArray.push(data);
-      console.log(myArray);
+      // console.log(myArray);
       div.innerHTML += `
-                <div class="row post" data-aos="flip-up">
-            <div class="read-img col-12 col-md-3 d-flex align-items-end border">
+        <div class="row my-3 post" data-aos="flip-up" data-id="${myArray[defaultItem].id}">
+            <div class="read-img col-12 col-md-3 d-flex align-items-end">
                 <img src="${imageURL.value}" alt="" class="post-image" />
             </div>
             <div class="read col-9 pe-md-5 mt-4 mt-md-0 d-flex flex-column justify-content-center">
                 <h3 class="fw-semibold">${myArray[defaultItem].title}</h3>
                 <p>
-                    by <span class="text-info pt-4">${writersArray[defaultItem].name} </span> ${dateArray[defaultItem].date}
+                  by <span class="text-info pt-4">${writersArray[defaultItem].name} </span> ${dateArray[defaultItem].date}
                 </p>
                 <div class="options d-flex align-items-center justify-content-between">
-                  <a href="./post.html" target="_blank">
-                    <button class="btn btn-info text-light">Read More</button>
-                  </a>
-                  <i class="del bi bi-trash3-fill fs-2 text-danger ms-5"></i>
+                  <div class="d-flex align-items-center">
+                    <i class="edit bi bi-pencil-square mx-3 fs-3 text-info" id="edit-post"></i>
+                    <a href="./post.html" target="_blank">
+                      <button class="btn btn-info text-light">Read More</button>
+                    </a>
+                  </div>
+                  <i class="del bi bi-trash3-fill fs-2 text-danger ms-5" id="delete-post"></i>
                 </div>
             </div>
         </div>
         `;
+
       overlay.classList.remove(`show-modal`);
-      inputBody.value = ``;
-      inputTitle.value = ``;
-      imageURL.value = ``;
+      form.reset();
       defaultItem++;
     });
+});
+
+console.log(div.childElementCount);
+
+// DELETE AND EDIT
+div.addEventListener(`click`, (e) => {
+  let deleteButtonIsPressed = e.target.id == `delete-post`;
+  let editButtonIsPressed = e.target.id == `edit-post`;
+  console.log(deleteButtonIsPressed);
+
+  let id = e.target.parentElement.parentElement.parentElement.dataset.id;
+  console.log(id);
+
+  if (deleteButtonIsPressed) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        let target =
+          e.target.parentElement.parentElement.parentElement.remove();
+      });
+  }
 });
 
 // GET POST FROM JSONPLACEHOLDER
@@ -106,7 +137,7 @@ getPost.addEventListener(`click`, () => {
       myArray = json;
       for (i = 0; i < 7; i++) {
         div.innerHTML += `
-                <div class="row post" data-aos="flip-up">
+                <div class="row my-3 post" data-aos="flip-up" data-id ="${myArray[i].id}">
             <div class="read-img col-12 col-md-3 d-flex align-items-end">
                 <img src="${pictureArray[i].img}" alt="" class="img-fluid post-image" />
             </div>
@@ -119,7 +150,7 @@ getPost.addEventListener(`click`, () => {
                   <a href="./post.html" target="_blank">
                     <button class="btn btn-info text-light">Read More</button>
                   </a>
-                  <i class="del bi bi-trash3-fill fs-2 text-danger ms-5"></i>
+                  <i class="bi bi-trash3-fill fs-2 text-danger ms-5" id="delete-post"></i>
                 </div>
             </div>
         </div>
